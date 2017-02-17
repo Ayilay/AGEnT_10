4f7b21dec11f0c8140f091086f9280ebadeb8f53
Ayilay <georgetroulis@gmail.com>
2017-02-16 19:50:39 -0800

    Fresh Start
    - Removed all old files
    - Removed MIT License from header, replaced with generic copyright for now
        - Will probably update software license later. Until then ...

--- AGEnDA/AGEnDA.ino
+++ AGEnDA/AGEnDA.ino
@@ -1,25 +1,5 @@
 /*
- * The MIT License (MIT)
- *
- * Copyright (c) 2015 George Troulis
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * Copyright (c) 2016 Georges Troulis
  *
  * Arduino
  * Game
@@ -27,18 +7,11 @@
  * Device for
  * Airsoft
  *
+ * TODO: Add description of wtf this is :)
+ *
  * Concept and Software by George Troulis
  * email: georgetroulis@gmail.com
 */
-
-// Include LiquidCrystal so that compiler doesn't freak out
-#include <LiquidCrystal.h>
-
-#include "GameInterface.h"
-
-GameInterface interface;
-
-int selection;
 
 void setup()
 {
@@ -46,6 +19,4 @@ void setup()
 
 void loop()
 {
-    selection = interface.showMenu();
-    interface.playGame(selection);
 }
